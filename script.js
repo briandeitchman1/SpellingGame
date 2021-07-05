5cbc9ec0517cabfe4c7a74fc109c2547942cb7e8
Brian Deitchman <briandeitchman@gmail.com>
2021-07-05 12:47:49 -0700

    Displays 1 random word at a time

--- script.js
+++ script.js
@@ -4,20 +4,25 @@ $(function(){
   $("button").click(function(){
     let userWord = $("#userWord").val();
     console.log(userWord + " debug");
-    if(userWord === words[0]){
+    if(userWord === words[rand]){
       alert("correct")
     }else{
       alert("incorrect")
     }
+    rand = getRandomInt(words.length);
+    $("#words").text(words[rand]);
   })
   $.get('test.txt', function(data){
-    words = data.split();
-    console.log(words[0]);
-    $("#words").text(words[0]);
+    words = data.split(" ");
+    rand = getRandomInt(words.length);
+    console.log(words[rand]);
+    $("#words").text(words[rand]);
   })
   $("#userWord").keypress(function(e){
-    console.log(e.target.value);
-    
-    
+    console.log(e.target.value); 
   })
+  // returns an int from 0 to max-1
+  function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
 })
